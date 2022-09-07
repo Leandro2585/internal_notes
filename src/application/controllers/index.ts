86430fb482c21c5a4c9b627ce1649d3eb9300bc4
leandro2585 <leo.real2585@gmail.com>
2022-09-07 19:50:35 -0300

    refactor: move create comment controller to its own file

--- src/application/controllers/index.ts
+++ src/application/controllers/index.ts
@@ -1,2 +1,3 @@
 export * from './load-posts-controller'
 export * from './create-post-controller'
+export * from './create-comment-controller'
--- tests/application/controllers/create-comment-controller.spec.ts
+++ tests/application/controllers/create-comment-controller.spec.ts
@@ -1,35 +1,7 @@
-import { Controller, HttpResponse } from '@application/protocols'
 import { CreateCommentUseCaseSpy, randomCharactersGenerator } from '../fakes'
-import { CreateCommentUseCase } from '@domain/usecases'
-import { CommentModel } from '@domain/models'
-import { success } from '@application/helper'
 import { ExcessiveCharactersError } from '@application/errors'
-
-export class CreateCommentController extends Controller {
-	constructor (private readonly createCommentUseCase: CreateCommentUseCase) { super() }
-
-	async execute({ comment, post_id, user_id }: CreateCommentController.Request): Promise<HttpResponse<CreateCommentController.Response>> {
-		const { comment: created_comment } = await this.createCommentUseCase.execute({ comment, post_id: Number(post_id), user_id: Number(user_id) })
-		return success({ comment: created_comment })
-	}
-  
-	validate({ comment }: CreateCommentController.Request): Error | undefined {
-		if(comment.length >= 777) return new ExcessiveCharactersError('comment', 777)
-		return undefined
-	}
-}
-
-export namespace CreateCommentController {
-  export type Request = {
-    comment: string
-    user_id: number
-    post_id: number
-  }
-
-  export type Response = {
-    comment: CommentModel
-  }
-}
+import { CreateCommentController } from '@application/controllers'
+import { Controller } from '@application/protocols'
 
 describe('create comment controller', () => {
 	let sut: CreateCommentController
