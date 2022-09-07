84586705d83760c8eedfea2958d3d9ce9dc8367b
leandro2585 <leo.real2585@gmail.com>
2022-09-07 18:59:11 -0300

    test: ensure pg users repository return user with the same user_id provided

--- tests/infra/database/repositories/pg-users-repository.spec.ts
+++ tests/infra/database/repositories/pg-users-repository.spec.ts
@@ -4,7 +4,7 @@ import { UserEntity } from '@infra/database/entities'
 import { PostgresConnection } from '@infra/database/helpers'
 import { makeFakeDatabase } from '../helpers/mock-database'
 import { LoadUserByIdRepository } from '@data/protocols/repositories'
-import { PostgresRepository } from '../protocols'
+import { PostgresRepository } from '@infra/database/protocols'
 
 export class PgUsersRepository extends PostgresRepository implements LoadUserByIdRepository {
 	async loadById({ user_id }: LoadUserByIdRepository.Input): Promise<LoadUserByIdRepository.Output> {
@@ -25,7 +25,7 @@ describe('pg users repository', () => {
 		const database = makeFakeDatabase([UserEntity])
 		backup = (await database).backup()
 		pgUsersRepository = connection.getRepository(UserEntity)
-  	})
+	})
 
 	afterAll(async () => {
 		connection.disconnect()
@@ -38,5 +38,24 @@ describe('pg users repository', () => {
   
 	test('should extend postgres repository', () => {
 		expect(sut).toBeInstanceOf(PostgresRepository)
+	})
+
+	test('should return user with the same user_id provided', async () => {
+		await pgUsersRepository.save({
+			id: 1,
+			name: 'USER',
+			created_at: new Date('2022-08-30 16:32:02'),
+			updated_at: new Date('2022-08-30 16:32:02'),
+		})
+		const result = await sut.loadById({ user_id: 1 })
+		
+		expect(result).toMatchObject({
+			user: {
+				id: 1,
+				name: 'USER',
+				created_at: new Date('2022-08-30 16:32:02'),
+				updated_at: new Date('2022-08-30 16:32:02'),
+			}
+		})
 	})
 })
